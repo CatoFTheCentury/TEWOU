70d0eec2c439144acaddceacc82440cfced35980
CatoFTheCentury <dagreat.dg@gmail.com>
2026-01-23 16:05:59 -0500

    Removed unused code

--- src/games/arbre/main.ts
+++ src/games/arbre/main.ts
@@ -1,21 +1,10 @@
 import {Game} from './game';
 import {Engine} from 'TEWOU'
-import {Alice} from './test2';
 import {Manager} from 'Console';
 
-// new Render.GL(600+'',600+'');
-// console.log("ALLO")
 let game = new Game(Manager.currentTarget);
-new Engine(game);
-
-new Alice().showResult();
 
 Engine.start(game).then(()=>{
   Engine.games.push(game);
   if(Engine.games.length<=1)Engine.mainLoop();
-  // else {
-  //   for(let i = 0; i < Engine.games.length-2;i++){
-  //     game.shareAlacrities(i);
-  //   }
-  // }
 })
