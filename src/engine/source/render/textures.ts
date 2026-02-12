6c40f63467d08dee8aaa299b5e1f823d532136a2
CatoFTheCentury <dagreat.dg@gmail.com>
2026-02-12 13:42:11 -0500

    Alpha set to premultiplied

--- src/engine/source/render/textures.ts
+++ src/engine/source/render/textures.ts
@@ -8,7 +8,9 @@ export default class Textures extends Render.Info {
     let gl = glContext.gl;
     let tex : WebGLTexture = Textures.createTexToBlitOn(glContext, img.width,img.height)
     gl.bindTexture(gl.TEXTURE_2D, tex)
+    gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img)
+    gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, false);
 
     return tex;
   }
